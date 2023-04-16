8938965008132a868db649fbb6576854febbdae3
Courtney Hampton <jaye0888@gmail.com>
2023-04-16 22:16:38 +0200

    crud operations

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -12,11 +12,15 @@ export class DashboardComponent implements OnInit {
   taskArr: Task[] = [];
 
   addTaskValue: string = '';
+  editTaskValue: string = '';
 
   constructor(private crudService: CrudService) {}
 
   ngOnInit(): void {
+    this.editTaskValue = '';
+    this.addTaskValue = '';
     this.taskObj = new Task();
+    this.taskArr = [];
     this.getAllTasks();
   }
 
@@ -24,7 +28,24 @@ export class DashboardComponent implements OnInit {
     this.crudService.getAllTasks().subscribe((res) => (this.taskArr = res));
   }
 
-  addTask(etask: Task) {
-    this.crudService.addTask(etask).subscribe((res) => this.ngOnInit());
+  addTask() {
+    this.taskObj.task_name = this.addTaskValue;
+    this.crudService.addTask(this.taskObj).subscribe((res) => {
+      this.ngOnInit();
+      this.addTaskValue = '';
+    });
+  }
+
+  editTask() {
+    this.taskObj.task_name = this.editTaskValue;
+    this.crudService.editTask(this.taskObj).subscribe((res) => this.ngOnInit());
+  }
+
+  deleteTask(etask: Task) {
+    this.crudService.deleteTask(etask).subscribe((res) => this.ngOnInit());
+  }
+  call(etask: Task) {
+    this.taskObj = etask;
+    this.editTaskValue = etask.task_name;
   }
 }
